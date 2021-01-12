beb44f6129d403f52b6c72b3208e559f6fa62ddf
Alonzo Turner <alonzo.turner@pedstores.com>
2021-01-12 23:50:07 +0000

    the first step in refactor

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -10,62 +10,64 @@ class GildedRose
 
     public $sellIn;
 
-    public function __construct($name, $quality, $sellIn)
+    public function __construct(string $name, int $quality, int $sellIn)
     {
         $this->name = $name;
         $this->quality = $quality;
         $this->sellIn = $sellIn;
     }
 
-    public static function of($name, $quality, $sellIn) {
+    public static function getItem($name, $quality, $sellIn) {
         return new static($name, $quality, $sellIn);
     }
 
-    public function tick()
+    public function numberOfDaysPassed(int $days): void
     {
-        if ($this->name != 'Aged Brie' and $this->name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if ($this->quality > 0) {
-                if ($this->name != 'Sulfuras, Hand of Ragnaros') {
-                    $this->quality = $this->quality - 1;
-                }
-            }
-        } else {
-            if ($this->quality < 50) {
-                $this->quality = $this->quality + 1;
-
-                if ($this->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                    if ($this->sellIn < 11) {
-                        if ($this->quality < 50) {
-                            $this->quality = $this->quality + 1;
-                        }
+        foreach(range(1, $days) as $count) {
+            if ($this->name != 'Aged Brie' and $this->name != 'Backstage passes to a TAFKAL80ETC concert') {
+                if ($this->quality > 0) {
+                    if ($this->name != 'Sulfuras, Hand of Ragnaros') {
+                        $this->quality = $this->quality - 1;
                     }
-                    if ($this->sellIn < 6) {
-                        if ($this->quality < 50) {
-                            $this->quality = $this->quality + 1;
-                        }
-                    }
-                }
-            }
-        }
-
-        if ($this->name != 'Sulfuras, Hand of Ragnaros') {
-            $this->sellIn = $this->sellIn - 1;
-        }
-
-        if ($this->sellIn < 0) {
-            if ($this->name != 'Aged Brie') {
-                if ($this->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                    if ($this->quality > 0) {
-                        if ($this->name != 'Sulfuras, Hand of Ragnaros') {
-                            $this->quality = $this->quality - 1;
-                        }
-                    }
-                } else {
-                    $this->quality = $this->quality - $this->quality;
                 }
             } else {
                 if ($this->quality < 50) {
                     $this->quality = $this->quality + 1;
+
+                    if ($this->name == 'Backstage passes to a TAFKAL80ETC concert') {
+                        if ($this->sellIn < 11) {
+                            if ($this->quality < 50) {
+                                $this->quality = $this->quality + 1;
+                            }
+                        }
+                        if ($this->sellIn < 6) {
+                            if ($this->quality < 50) {
+                                $this->quality = $this->quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if ($this->name != 'Sulfuras, Hand of Ragnaros') {
+                $this->sellIn = $this->sellIn - 1;
+            }
+
+            if ($this->sellIn < 0) {
+                if ($this->name != 'Aged Brie') {
+                    if ($this->name != 'Backstage passes to a TAFKAL80ETC concert') {
+                        if ($this->quality > 0) {
+                            if ($this->name != 'Sulfuras, Hand of Ragnaros') {
+                                $this->quality = $this->quality - 1;
+                            }
+                        }
+                    } else {
+                        $this->quality = $this->quality - $this->quality;
+                    }
+                } else {
+                    if ($this->quality < 50) {
+                        $this->quality = $this->quality + 1;
+                    }
                 }
             }
         }
--- tests/GildedRoseTest.php
+++ tests/GildedRoseTest.php
@@ -9,84 +9,75 @@ class GildedRoseTest extends TestCase
 {
     public function testThatAnItemHasSellIn(): void
     {
-        $item = GildedRose::of('anything', 4, 4);
+        $item = GildedRose::getItem('anything', 4, 4);
         $this->assertObjectHasAttribute('sellIn', $item);
     }
 
     public function testThatAnItemHasQuality(): void
     {
-        $item = GildedRose::of('eggplant', 4, 5);
+        $item = GildedRose::getItem('eggplant', 4, 5);
         $this->assertObjectHasAttribute('quality', $item);
     }
 
     public function testThatAnItemQualityAndSellInDecreaseEachDay(): void
     {
-        $item = GildedRose::of('apple', 4, 4);
-        $item->tick();
+        $item = GildedRose::getItem('apple', 4, 4);
+        $item->numberOfDaysPassed(1);
         $this->assertTrue($item->quality === 3);
         $this->assertTrue($item->sellIn === 3);
     }
 
     public function testThatQualityDegradesTwiceAsFastAfterSellIn(): void
     {
-        $item = GildedRose::of('persimmon', 6, 2);
-        $item->tick();
-        $item->tick();
+        $item = GildedRose::getItem('persimmon', 6, 2);
+        $item->numberOfDaysPassed(2);
         $this->assertTrue($item->quality === 4);
-        $item->tick();
+        $item->numberOfDaysPassed(1);
         $this->assertTrue($item->quality === 2);
     }
 
     public function testThatAgedBrieIncreasesInQualityAsItAges(): void
     {
-        $item = GildedRose::of('Aged Brie', 4, 4);
-        $item->tick();
-        $item->tick();
+        $item = GildedRose::getItem('Aged Brie', 4, 4);
+        $item->numberOfDaysPassed(2);
         $this->assertTrue($item->quality === 6);
     }
+
     public function testTheQualityOfAnItemIsNeverMoreThanFifty(): void
     {
-        $item = GildedRose::of('Aged Brie', 48, 1);
-        $item->tick();
-        $item->tick();
-        $item->tick();
+        $item = GildedRose::getItem('Aged Brie', 48, 1);
+        $item->numberOfDaysPassed(3);
         $this->assertTrue($item->quality === 50, $item->quality);
 
-//        $item = GildedRose::of('pineapple', 100, 4);
-//        $this->assertTrue($item->quality === 50);
+        $item = GildedRose::getItem('pineapple', 100, 4);
+        $this->assertTrue($item->quality === 50);
     }
 
     public function testThatSulfurasNeverHasToBeSoldAndNeverDecreasesInQuality(): void
     {
-        $item = GildedRose::of('Sulfuras, Hand of Ragnaros', 50, 4);
-        $item->tick();
-        $item->tick();
-        $item->tick();
+        $item = GildedRose::getItem('Sulfuras, Hand of Ragnaros', 50, 4);
+        $item->numberOfDaysPassed(3);
         $this->assertTrue($item->quality === 50);
         $this->assertTrue($item->sellIn === 4);
     }
 
     public function testThatBackStagePassesIncreaseInQualityEtAl(): void
     {
-        $item = GildedRose::of('Backstage passes to a TAFKAL80ETC concert', 5, 12);
-        $item->tick();
-        $item->tick();
+        $item = GildedRose::getItem('Backstage passes to a TAFKAL80ETC concert', 5, 12);
+        $item->numberOfDaysPassed(2);
+        // after two days quality has increased two
         $this->assertTrue($item->quality === 7);
         $this->assertTrue($item->sellIn === 10);
-        $item->tick();
-        $item->tick();
+        $item->numberOfDaysPassed(2);
+        // with 10 days sellIn left quality increases by 2
         $this->assertTrue($item->quality === 11);
-        $item->tick();
-        $item->tick();
-        $item->tick();
-        $item->tick();
+        $item->numberOfDaysPassed(4);
+        // with less than 5 sellIn left quality increases by 3
         $this->assertTrue($item->quality === 20);
-        $item->tick();
-        $item->tick();
-        $item->tick();
-        $item->tick();
+        $item->numberOfDaysPassed(4);
         $this->assertTrue($item->quality === 32);
-        $item->tick();
+        $item->numberOfDaysPassed(1);
+        // when the sellIn date has passed quality drops to zero
         $this->assertTrue($item->quality === 0);
         $this->assertTrue($item->sellIn === -1);
     }
