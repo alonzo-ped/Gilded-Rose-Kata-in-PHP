f90ee272126e152ea04a808dcddaefe83bd769db
Alonzo Turner <alonzo.turner@pedstores.com>
2021-01-12 23:17:19 +0000

    add second unit test

--- tests/GildedRoseTest.php
+++ tests/GildedRoseTest.php
@@ -12,4 +12,10 @@ class GildedRoseTest extends TestCase
         $item = GildedRose::of('anything', 4, 4);
         $this->assertObjectHasAttribute('sellIn', $item);
     }
+
+    public function testThatAnItemHasQuality(): void
+    {
+        $item = GildedRose::of('eggplant', 4, 5);
+        $this->assertObjectHasAttribute('quality', $item);
+    }
 }
